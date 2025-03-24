d36e97b07e8c2728bb91a5ca5348d2466282e356
sebaditucci <sebastiandit240@gmail.com~>
2025-03-24 19:19:12 -0300

    style(home): Cambios en el ordenamiento de componentes

--- src/HomeComponents/Content.jsx
+++ src/HomeComponents/Content.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import Productos from './Productos'
 import Oferta from './Oferta'
 import FAQ from './FAQ'
+import styles from '../HomeStyles/content.module.css'
 
 
 export default function Content() {
   return (
-    <main style = {{marginTop: "50px", marginBottom: "50px"}}>
+    <main className = {styles.content}>
         <Productos titulo = "Productos Destacados" subtitulo= "✅ Calidad y tendencia en un solo lugar" clase = "tendencia"/>
         <Oferta titulo = "Campera Universitaria en Oferta - 25% 🔥" 
         descripcion = "✨ Diseño cómodo, ligero y resistente para salir con estilo. Disponible en varios colores." precioRegular = "$50.000" 
         precioDescuento= "$37.000" bonus = "🚚 ¡Envío GRATIS a todo el país si compras antes del domingo!"
         tiempo = "⚠️ ¡Stock limitado! Aprovechá la promo antes de que se acabe."/>
         <Productos titulo = "Nuevos Lanzamientos" subtitulo= "🆕 Recién Llegados: Lo Nuevo en Moda te Espera!" clase = "nuevo"/>
-        <FAQ/>
         <Productos titulo = "Más Vendidos" subtitulo= "Los favoritos de nuestros clientes 🔥 ¡No te quedes sin el tuyo!" clase = "mas_vendidos"/>
+        <FAQ/>
     </main>
   )
 }
---
+++ src/Pages/ProductosPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductosPage() {
+  return (
+    <div>ProductosPage</div>
+  )
+}
